1e90cefc2ca09472ce0c4fea543c1ea8f542f0a9
kelvinkirima014 <kelvinkirima2@gmail.com>
2023-10-13 08:48:59 +0300

    rename forge-core to backend

--- forge-core/target/debug/deps/forge_core-7cbc979c544374a2.d
+++
@@ -1,5 +0,0 @@
-/home/0xAckerman/Projects/rustyforge/forge-core/target/debug/deps/forge_core-7cbc979c544374a2.rmeta: src/lib.rs
-
-/home/0xAckerman/Projects/rustyforge/forge-core/target/debug/deps/forge_core-7cbc979c544374a2.d: src/lib.rs
-
-src/lib.rs:
