219cefab9b976da53ae9a1ef2b1e54b7e2db9b27
kelvinkirima014 <kelvinkirima2@gmail.com>
2023-10-21 07:19:10 +0300

    add Reasource trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,21 @@
+use std::time::SystemTime;
+
+///The Core Rustyforge API
+/// 
+/// Resource allows us to handle different kinds of resources(like HTML,
+/// CSS, Images, Json, etc.) in a more uniform way.
 trait Resource {
+    ///allows each implementor of `Resource` to specify
+    /// what kind of output they produce.
     type Output;
+    ///Return whether the resource has been modified or not
     fn modified(&self) -> Modified;
+    ///actually produces the output
     fn generate(&self) -> Self::Output;
 }
 
+///Indicates the modification state of a `Resource`
 pub enum Modified {
-    
+    Never,
+    At(SystemTime)
 }
